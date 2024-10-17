0a5721b3d78fa07643378ad60ff1c0cf84d2cb70
Serendipitiousz <lai.hanyu@yahoo.com>
2024-10-17 13:25:21 +0800

    Update code

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -4,7 +4,7 @@ import { ShopContext } from '../../context/shopContext.jsx';
 import LazyLoad from 'react-lazyload';
 
 export const Product = (props) => {
-  const { addToCart, cartItem } = useContext(ShopContext);
+  const { addToCart, cartItem ,removeFromCart} = useContext(ShopContext);
   const { id, price, images, title } = props.data;
 
   const cartItemAmount = cartItem[id] || 0;
@@ -17,9 +17,13 @@ export const Product = (props) => {
       
       <h3>{title}</h3>
       <p>${price}</p>
-      <button className='add-cart' onClick={() => addToCart(id)}>
-        Add To Cart {cartItemAmount > 0 && `(${cartItemAmount})`}
-      </button>
+      <div className='btn-div'>
+        <button className='add-cart' onClick={() => addToCart(id)}>
+          Add To Cart {cartItemAmount > 0 && `(${cartItemAmount})`}
+        </button>
+        {cartItemAmount > 0 && <button className='add-cart' onClick={() => removeFromCart(id)}>-</button>}
+      </div>
+     
     </div>
   );
 };
